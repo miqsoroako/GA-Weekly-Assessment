b85237791c4417f694fa7973b2d2b0eb16c01f2e
miqsoroako <91551361+miqsoroako@users.noreply.github.com>
2021-10-04 21:09:10 +0700

    Update week1.js

--- week1.js
+++ week1.js
@@ -1,12 +1,14 @@
 const myAge = age => {
     let str = ''
-    if (age > 60) {str = 'tua'}
-    else if (age > 25) {str = 'dewasa'}
-    else if (age > 12) {str = 'remaja'}
-    else if (age > 0) {str = 'muda'}
-    else {str = 'error'}
+    if (age > 60) {str = range[0]}
+    else if (age > 25) {str = range[1]}
+    else if (age > 12) {str = range[2]}
+    else if (age > 0) {str = range[3]}
+    else {str = range[4]}
     console.log(`kamu ${str}`)
 }
+
+var range = ['tua', 'dewasa', 'remaja', 'muda', 'error']
 
 myAge(2)        // output: ''kamu muda'
 myAge(15)       // output: ''kamu remaja'
